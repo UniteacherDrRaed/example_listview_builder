bb5a3fee470d6cc1018378f7620e4e4d9daba527
Dr.Raed <drraed.jb@gmail.com>
2023-11-01 06:57:38 +0200

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -54,12 +54,32 @@ final List<Color> listOfColors =[
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ListView.builder example",
-        style: TextStyle(color:Colors.white,
-            fontSize: 24,fontWeight:
-            FontWeight.bold),),
+        backgroundColor: Colors.white,
+        title: Container(
+          width: 350,
+          height: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              colors: listOfColors,
+            ),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(6,7)
+              )
+            ]
+          ),
+          child:  const Center(
+            child: Text("ListView.builder example",
+            style: TextStyle(color:Colors.white,
+                fontSize: 24,fontWeight:
+                FontWeight.bold),),
+          ),
+        ),
         centerTitle: true,
-        toolbarHeight:100,
+        toolbarHeight:75,
+
       ),
       body: ListView.builder(
               itemCount: listOfColors.length,
@@ -67,9 +87,9 @@ final List<Color> listOfColors =[
                 return ListTile(
                   onTap: (){
                     ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(duration: Duration(seconds: 1),
+                        SnackBar(duration:const Duration(seconds: 1),
                             backgroundColor: listOfColors[index],
-                            content: Text("product $index with price ${index*100}",
+                            content: Text("product $index with price ${index*100+500}",
                               style: const TextStyle(color:Colors.white,
                                   fontSize: 24),
                             )));
@@ -77,7 +97,7 @@ final List<Color> listOfColors =[
                   tileColor: listOfColors[index],
                   title: Text("Product $index"),
                   leading: const Icon(Icons.laptop,color: Colors.white,),
-                  trailing: Text("price ${index*100}"),
+                  trailing: Text("price ${index*100+500}"),
                   subtitle: const Text("specification"),
                   textColor: Colors.white,
                   iconColor: Colors.white,
